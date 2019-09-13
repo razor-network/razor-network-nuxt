7d17cdcd1a8b5f0ba139ddc424c5eb73e48fbcbe
rpk79 <rajkharvar1808@gmail.com>
2019-09-14 00:00:41 +0530

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header_2 bg-dark" style="height: 40vh">
+    <h1
+      class="color-white text-center"
+      data-aos-duration="500"
+      data-aos="fade-down"
+      data-aos-delay="0"
+    >404 error</h1>
+    <h2 class="color-white text-center">This Page doesn't exist</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
